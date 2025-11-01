01a836aeb703ff5bb52fa12657d12d020f91afd2
Nakhwah Fadiyatul Abidah <nakhwah29@gmail.com>
2025-11-01 14:55:46 +0800

    Tampilan
    Tampilan

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home.index');
+});
+
+Route::get('/about', function () {
+    return view('home.about');
+});
+
+Route::get('/blog', function () {
+    return view('home.blog');
+});
+
+Route::get('/contact', function () {
+    return view('home.contact');
+});
+
+Route::get('/elements', function () {
+    return view('home.elements');
+});
+
+Route::get('/main', function () {
+    return view('home.main');
+});
+
+Route::get('/recipes_details', function () {
+    return view('home.recipes_details');
+});
+
+Route::get('/recipes', function () {
+    return view('home.recipes');
+});
+
+Route::get('/single-blog', function () {
+    return view('home.single-blog');
+});
