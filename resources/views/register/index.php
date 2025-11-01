c9d125dfbe49de479c3a3600364a64d8209090ab
RahmadHidayat1038 <10221038@student.itk.ac.id>
2025-11-01 18:08:51 +0800

    menambah register
    menambah register

--- resources/views/register/index.php
+++ resources/views/register/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login</title>
+	<title>Register</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -32,8 +32,16 @@
 
 				<form class="login100-form validate-form">
 					<span class="login100-form-title">
-						Login
+						Register
 					</span>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Username is required">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
@@ -52,8 +60,8 @@
 					</div>
 
                     <div class="container-login100-form-btn">
-                        <button type="button" class="login100-form-btn" onclick="window.location.href='index.php'">
-                            Login
+                        <button type="button" class="login100-form-btn" onclick="window.location.href='login'">
+                            Register
                         </button>
                     </div>
 
@@ -67,8 +75,8 @@
 					</div>
 
 					<div class="text-center p-t-136">
-						<a class="txt2" href="register">
-							Create your Account
+						<a class="txt2" href="login">
+							Already have Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ Route::get('/login', function () {
     return view('login.index');
 });
 
+Route::get('/register', function () {
+    return view('register.index');
+});
+
+
 Route::get('/', function () {
     return view('home.index');
 });
