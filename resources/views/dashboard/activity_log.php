db14c851e79b7703f1ec4452f44195e2d1008298
Nakhwah Fadiyatul Abidah <nakhwah29@gmail.com>
2025-11-25 10:49:57 +0800

    .
    .

--- resources/views/dashboard/activity_log.php
+++ resources/views/dashboard/activity_log.php
@@ -212,7 +212,7 @@
   <script src="../assets/js/core/bootstrap.min.js"></script>
   <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script type="module" src="../assets/js/action_log.js"></script>
+  <script type="module" src="../assets/js/activity_log.js"></script>
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
